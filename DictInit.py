7eed890423df9650fc9dcf8e4c0f58b12da4cffe
Lobanov Anton <alobanov@astralinux.ru>
2020-04-20 03:59:52 +0300

    add new file

---
+++ DictInit.py
@@ -0,0 +1,10 @@
+arrDist = {
+    "name": "Anton Lobanov",
+    "email": "alobanov@astralinux.ru",
+    "age": 37,
+    "boolValue": True
+}
+
+arrDist["car"] = "Niva"
+print(arrDist.get("car"))
+print(arrDist)
