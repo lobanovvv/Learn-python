307252905e5d4386093105a6067c46dda589d8b8
Lobanov Anton <alobanov@astralinux.ru>
2020-04-29 20:25:05 +0300

    add inheritance class

--- turtle_something.py
+++ turtle_something.py
@@ -1,32 +1,39 @@
 import turtle
 
-mark = turtle.Turtle()
+class Moves(turtle.Turtle):
+
+  def circle_v2(self):
+    for i in range(360):
+      self.forward(1)
+      self.left(1)
+
+  def circle_v1(self, diamert):
+    self.circle(diamert)
+
+  def square(self, size):
+    for i in range(4):
+      self.forward(size)
+      self.left(90)
+
+  def more_square(self, total_square, size, move_out):
+    for iteration in range(total_square):
+      self.square(size)
+      self.penup()
+      self.backward(move_out)
+      self.right(90)
+      self.forward(move_out)
+      self.left(90)
+      self.pendown()
+      size += move_out*2
+
+
+mark = Moves()
 mark.speed(11)
 mark.shape('turtle')
 
-def circle_v2():
-  for i in range(360):
-    mark.forward(1)
-    mark.left(1)
-
-def circle(diamert):
-  mark.circle(diamert)
-
-def square(size):
-  for i in range(4):
-    mark.forward(size)
-    mark.left(90)
-
-def more_square(total_square, size, move_out):
-  for iteration in range(total_square):
-    square(size)
-    mark.penup()
-    mark.backward(move_out)
-    mark.right(90)
-    mark.forward(move_out)
-    mark.left(90)
-    mark.pendown()
-    size += move_out*2
     
+mark.more_square(total_square = 10, size = 10, move_out = 10)
+#mark.square(44)
+#mark.circle_v2()
+#mark.more_square(10,22,22)
 
-more_square(10,22,22)
