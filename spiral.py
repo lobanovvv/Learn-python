e2eeae5d660752f1f1f3dc32c763db2d29e101f0
Lobanov Anton <alobanov@astralinux.ru>
2020-05-03 00:37:20 +0300

    rewrite spiral and add to turtle multi_square method

--- spiral.py
+++ spiral.py
@@ -1,1 +1,17 @@
-#put here spiral
+import turtle
+
+
+def spiral(count):
+    
+    n = .1
+    
+    for count_of_circle in range(count):
+        for i in range(360):
+            n += .001
+            mark.forward(n)
+            mark.left(1)
+
+
+mark = turtle.Turtle()
+mark.speed(100)
+spiral(10)
--- turtle_something.py
+++ turtle_something.py
@@ -2,10 +2,13 @@ import turtle
 
 class Moves(turtle.Turtle):
 
-  def circle_v2(self):
-    for i in range(360):
-      self.forward(1)
-      self.left(1)
+  def circle_v2(self, count):
+    n = .1
+    for count_of_circle in range(count):
+      for i in range(360):
+        n += .001
+        self.forward(n)
+        self.left(1)
 
   def circle_v1(self, diamert):
     self.circle(diamert)
@@ -26,13 +29,22 @@ class Moves(turtle.Turtle):
       self.pendown()
       size += move_out*2
 
+  def multi_square(self, count, length):
+    len = length
+    for i in range(count):
+      self.forward(len)
+      self.left(90)
+      self.forward(len)
+      self.left(90)
+      len += length
+
 
 mark = Moves()
-mark.speed(11)
+mark.speed(33)
 mark.shape('turtle')
 
-    
-mark.more_square(total_square = 10, size = 10, move_out = 10)
+mark.multi_square(22, 22)
+#mark.more_square(total_square = 10, size = 10, move_out = 10)
 #mark.square(44)
 #mark.circle_v2()
 #mark.more_square(10,22,22)
