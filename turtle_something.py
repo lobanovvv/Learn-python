c7768420ce8d12e63a12c1df1efefb6edfae394d
Lobanov Anton <alobanov@astralinux.ru>
2020-05-04 00:08:01 +0300

    add multi_polygon method

--- turtle_something.py
+++ turtle_something.py
@@ -38,12 +38,73 @@ class Moves(turtle.Turtle):
       self.left(90)
       len += length
 
+  def multi_polygon(self, polygons_count, size):
+
+    #Draw figure
+    def figure(turn_before_draw, corners, mod_size):
+      self.left(turn_before_draw)
+      for line in range(corners):
+        self.left(360.0 / corners)
+        self.forward(size + mod_size)
+
+    #Make indent for draw next figure
+    def indent(turn_right):
+      self.penup()
+      self.right(turn_right)
+      self.forward(16)
+      self.pendown()
+
+    #Draw cross line for cheking center of all figure
+    def check_center():
+      self.right(72.5)
+      self.backward(280 / 2)
+      self.left(90)
+      self.color("red")
+      self.forward(150)
+      self.backward(300)
+      self.forward(150)
+      self.right(90)
+      self.color("black")
+      self.backward(280 / 2)
+        
+    #Triangle
+    figure(30, 3, -5)
+    indent(30)
+
+    #4-side polygon
+    figure(45, 4, 10)
+    indent(45)
+
+    #5-side polygon
+    figure(55, 5, 20)
+    indent(55)
+
+    #6-side polygon
+    figure(60, 6, 25)
+    indent(60)
+
+    #7-side polygon
+    figure(65, 7, 28)
+    indent(65)
+
+    #8-side polygon
+    figure(67.5, 8, 30)
+    indent(67.5)
+
+    #9-side polygon
+    figure(70, 9, 31)
+    indent(70)
+
+    #10-side polygon
+    figure(72.5, 10, 32)
+
 
 mark = Moves()
-mark.speed(33)
+mark.speed(5)
 mark.shape('turtle')
 
-mark.multi_square(22, 22)
+mark.multi_polygon(polygons_count = 3, size = 55)
+#mark.multi_square(22, 22)
 #mark.more_square(total_square = 10, size = 10, move_out = 10)
 #mark.square(44)
 #mark.circle_v2()
