efb713d9be03d74f703d40bfbfd43b76b497c320
Anton Lobanov <lobanovvv@mail.ru>
2020-10-24 17:23:24 +0300

    new files

--- Unittest module/calc.py
+++ Unittest module/calc.py
@@ -1,17 +1,17 @@
 def add(x, y):
-    “””Add Function”””
+    """Add Function"""
     return x + y
 
 def subtract(x, y):
-    “””Subtract Function”””
+    """Subtract Function"""
     return x - y
 
 def multiply(x, y):
-    “””Multiply Function”””
+    """Multiply Function"""
     return x * y
 
 def divide(x, y):
-    “””Divide Function”””
+    """Divide Function"""
     if y == 0:
-        raise ValueError(‘Can not divide by zero’)
+        raise ValueError('Can not divide by zero')
     return x / y
